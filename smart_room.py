34d1b3982a90ecb7b159beb63c0d3c4355a2ab7f
Jay Paul Aying <jaypaulaying@Johns-MacBook-Pro-2.local>
2019-01-20 18:23:45 +0800

    serial read

--- smart_room.py
+++ smart_room.py
@@ -1,26 +1,40 @@
 #!/usr/bin/python
 
-from Tkinter import *   ## notice capitalized T in Tkinter 
+import Tkinter as tk ## notice capitalized T in Tkinter 
 import time
+import threading
+import Queue
 import serial
 
-class Login():
 
-    def __init__(self, master):
+class SerialThread(threading.Thread):
+    def __init__(self, queue):
+        threading.Thread.__init__(self)
+        self.queue = queue
+    def run(self):
+        while True:
+            if ser.inWaiting():
+                text = ser.readline(ser.inWaiting())
+                self.queue.put(text)
+
+
+class Login(tk.Tk):
+
+    def __init__(self):
         # defines a grid 50 x 50 cells in the main window
+        tk.Tk.__init__(self)
 
-        self.master=master
-        self.master.title('Smart Room Login')
-        self.master.geometry('400x250')
+        self.title('Smart Room Login')
+        self.geometry('400x250')
         self.rows = 0
         while self.rows < 10:
-            self.master.rowconfigure(self.rows, weight=1)
-            self.master.columnconfigure(self.rows, weight=1)
+            self.rowconfigure(self.rows, weight=1)
+            self.columnconfigure(self.rows, weight=1)
             self.rows += 1
  
  
         # adds username entry widget and defines its properties
-        self.username_box = Entry(self.master)
+        self.username_box = tk.Entry(self)
         self.username_box.insert(0, 'Enter Username')
         self.username_box.bind('<FocusIn>', self.clear_widget)
         self.username_box.bind('<FocusOut>', self.repopulate_defaults)
@@ -28,7 +42,7 @@ class Login():
          
          
         # adds password entry widget and defines its properties
-        self.password_box = Entry(self.master, show='*')
+        self.password_box = tk.Entry(self, show='*')
         self.password_box.insert(0, '     ')
         self.password_box.bind("<FocusIn>", self.clear_widget)
         self.password_box.bind('<FocusOut>', self.repopulate_defaults)
@@ -37,17 +51,17 @@ class Login():
          
          
         # adds login button and defines its properties
-        self.login_btn = Button(self.master, text='Login', command=self.login)
+        self.login_btn = tk.Button(self, text='Login', command=self.login)
         self.login_btn.bind('<Return>', self.login)
         self.login_btn.grid(row=5, column=5, sticky='NESW')
 
         # adds login button and defines its properties
-        self.quit_btn = Button(self.master, text='Quit', command=self.quit)
+        self.quit_btn = tk.Button(self, text='Quit', command=self.quit)
         self.quit_btn.bind('<Return>', self.quit)
         self.quit_btn.grid(row=6, column=5, sticky='NESW')
 
-        self.login_text = StringVar()
-        self.login_label = Label(self.master, textvariable=self.login_text)
+        self.login_text = tk.StringVar()
+        self.login_label = tk.Label(self, textvariable=self.login_text)
         self.login_label.grid(row=7, column=5, sticky='NESW')
  
  
@@ -55,18 +69,18 @@ class Login():
      
         # will clear out any entry boxes defined below when the user shifts
         # focus to the widgets defined below
-        if self.username_box == self.master.focus_get() and self.username_box.get() == 'Enter Username':
-            self.username_box.delete(0, END)
+        if self.username_box == self.focus_get() and self.username_box.get() == 'Enter Username':
+            self.username_box.delete(0, tk.END)
         elif self.password_box == self.password_box.focus_get() and self.password_box.get() == '     ':
-            self.password_box.delete(0, END)
+            self.password_box.delete(0, tk.END)
      
     def repopulate_defaults(self, event):
      
         # will repopulate the default text previously inside the entry boxes defined below if
         # the user does not put anything in while focused and changes focus to another widget
-        if self.username_box != self.master.focus_get() and self.username_box.get() == '':
+        if self.username_box != self.focus_get() and self.username_box.get() == '':
             self.username_box.insert(0, 'Enter Username')
-        elif self.password_box != self.master.focus_get() and self.password_box.get() == '':
+        elif self.password_box != self.focus_get() and self.password_box.get() == '':
             self.password_box.insert(0, '     ')
      
     def login(self, *event):
@@ -77,21 +91,22 @@ class Login():
 
         if self.username_box.get() == 'admin' and self.password_box.get() == '1234':
             self.login_text.set("Credentials correct.")
-            self.master.withdraw()
-            root2 = Toplevel(self.master)
+            root2 = tk.Toplevel(self)
             smartRoomDashboard = Dashboard(root2)
+            self.destroy()
         else:
             self.login_text.set("Unable to login with those credentials.")
      
        
 
     def quit(self, *event):
-        self.master.destroy()
+        self.destroy()
 
-class Dashboard():
+class Dashboard(tk.Tk):
     
 
     def __init__(self, master):
+        tk.Tk.__init__(self)
         self.lt0 = False
         self.lt1 = False
         self.lt2 = False
@@ -100,26 +115,25 @@ class Dashboard():
         self.overridest = False
 
 
-        self.master=master
-        self.master.title('Smart Room Dashboard')
-        self.master.geometry('400x250')
+        self.title('Smart Room Dashboard')
+        self.geometry('400x350')
 
         self.rows = 0
         while self.rows < 10:
-            self.master.rowconfigure(self.rows, weight=1)
-            self.master.columnconfigure(self.rows, weight=1)
+            self.rowconfigure(self.rows, weight=1)
+            self.columnconfigure(self.rows, weight=1)
             self.rows += 1
 
 
-        self.labelframe0 = LabelFrame(self.master, text="Override System")
+        self.labelframe0 = tk.LabelFrame(self, text="Override System")
         self.labelframe0.grid(row=1, column=1, sticky='NESW')
         self.labelframe0.columnconfigure(1, weight=1)
 
 
-        self.override_btn = Button(self.labelframe0, text='Override', command=self.override)
+        self.override_btn = tk.Button(self.labelframe0, text='Override', command=self.override)
         self.override_btn.grid(row=0, column=0, columnspan=2, sticky='NESW')
 
-        self.labelframe1 = LabelFrame(self.master, text="Lights Control")
+        self.labelframe1 = tk.LabelFrame(self, text="Lights Control")
         self.labelframe1.grid(row=2, column=1, sticky='NESW')
         self.labelframe1.columnconfigure(1, weight=1)
 
@@ -127,26 +141,49 @@ class Dashboard():
         # self.login_label.grid(row=2, column=0, sticky='NESW')
 
 
-        self.light0_btn = Button(self.labelframe1, text='Light 1', command=lambda:self.light_control(0) )
+        self.light0_btn = tk.Button(self.labelframe1, text='Light 1', command=lambda:self.light_control(0) )
         self.light0_btn.grid(row=0, column=0, columnspan=2,sticky='NESW')
 
-        self.light1_btn = Button(self.labelframe1, text='Light 2', command=lambda:self.light_control(1))
+        self.light1_btn = tk.Button(self.labelframe1, text='Light 2', command=lambda:self.light_control(1))
         self.light1_btn.grid(row=1, column=0, columnspan=2, sticky='NESW')
 
-        self.light2_btn = Button(self.labelframe1, text='Light 3', command=lambda:self.light_control(2))
+        self.light2_btn = tk.Button(self.labelframe1, text='Light 3', command=lambda:self.light_control(2))
         self.light2_btn.grid(row=2, column=0, columnspan=2, sticky='NESW')
 
-        self.light3_btn = Button(self.labelframe1, text='Light 4', command=lambda:self.light_control(3))
+        self.light3_btn = tk.Button(self.labelframe1, text='Light 4', command=lambda:self.light_control(3))
         self.light3_btn.grid(row=3, column=0, columnspan=2, sticky='NESW')
 
-        self.lightAll_btn = Button(self.labelframe1, text='All Lights', command=lambda:self.light_control(4))
+        self.lightAll_btn = tk.Button(self.labelframe1, text='All Lights', command=lambda:self.light_control(4))
         self.lightAll_btn.grid(row=4, column=0, columnspan=2, sticky='NESW')
 
 
 
+        self.labelframe2 = tk.LabelFrame(self, text="Thermal Sensor Data")
+        self.labelframe2.grid(row=1, column=3, columnspan=2, sticky='NESW')
+        self.labelframe2.columnconfigure(4, weight=1)
+        self.labelframe2.rowconfigure(4, weight=1)
+
+        self.k = 1
+        for self.i in  range(4):
+            for self.j in  range(4):
+                tk.Button(self.labelframe2, text=str(self.k)).grid(row=int(self.i), column = int(self.j))
+                self.j = self.j + 1
+                self.k = self.k +1 
+            self.i = self.i + 1
+
+
+
+
+
         # adds login button and defines its properties
-        self.back_btn = Button(self.master, text='Back', command=self.back)
+        self.back_btn = tk.Button(self, text='Back', command=self.back)
         self.back_btn.grid(row=8, column=4, sticky='NESW')
+
+
+        self.queue = Queue.Queue()
+        thread = SerialThread(self.queue)
+        thread.start()
+        self.process_serial()
 
     def light_control(self, light):
         if self.lt0 and self.lt1 and self.lt2 and self.lt3:
@@ -216,15 +253,27 @@ class Dashboard():
         time.sleep(1)
 
     def back(self):
-        self.master.withdraw()
-        root2 = Toplevel(self.master)
-        smartRoom = Login(root2)
+        smartRoom = Login()
+        self.destroy()
          
+    def process_serial(self):
+        while self.queue.qsize():
+            try:
+                # self.text.delete(1.0, 'end')
+                # self.text.insert('end', self.queue.get())
+                print self.queue.get()
+            except Queue.Empty:
+                pass
+        self.after(100, self.process_serial)
+
 
 def main():
-    root = Tk();
-    smartRoom=Login(root)
-    root.mainloop()
+    app = Login()
+    app.mainloop()
+
+    # root = Tk();
+    # smartRoom=Login(root)
+    # root.mainloop()
 
 if __name__ == '__main__':
 
