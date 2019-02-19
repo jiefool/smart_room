abca5020e371e11ee98981c559e69c7e1aad56d5
Jay Paul Aying <jaypaulaying5@gmail.com>
2019-02-19 20:58:12 +0800

    uncoment ser

--- smart_room.py
+++ smart_room.py
@@ -478,14 +478,14 @@ def main():
 
 if __name__ == '__main__':
 
-    # ser = serial.Serial(
-    #     port='/dev/ttyUSB0',
-    #     baudrate=9600,
-    #     parity=serial.PARITY_NONE,
-    #     stopbits=serial.STOPBITS_ONE,
-    #     bytesize=serial.EIGHTBITS,
-    #     timeout=1
-    # )
+    ser = serial.Serial(
+        port='/dev/ttyUSB0',
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+    )
 
 
     mydb = mysql.connector.connect(
